13467b7f698304efe6c32a2605669a685e22bc37
bestdevmgp <me@mingyu.dev>
2025-10-13 20:12:32 +0900

    feat: education active status

--- app/_components/EducationCard.tsx
+++ app/_components/EducationCard.tsx
@@ -1,21 +1,20 @@
 import { education } from "@prisma/client";
+import cn from "classnames";
 import parse from "html-react-parser";
+
+import Shape from "@/assets/shape-sparkle.svg";
 
 interface EducationCardProps extends education {}
 
-const EducationCard = ({ id, period, title, sub_title, items }: EducationCardProps) => {
+const EducationCard = ({ id, period, title, sub_title, items, is_active }: EducationCardProps) => {
   return (
     <div key={`edu-card-${id}`} className="grid sm:grid-cols-3 sm:gap-x-10 sm:items-start">
-      <p
-        className="
-          text-sm md:text-base text-foreground/60 mb-3 sm:ml-auto
-          before:content-['*'] before:text-foreground/30 before:w-[21px] before:inline-block
-        "
-      >
-        {period}
-      </p>
+      <div className="flex gap-2.5 sm:justify-end items-center mb-3">
+        <Shape className={cn(is_active ? "text-primary" : "text-foreground/30")} />
+        <p className="text-sm md:text-base font-normal text-foreground/60">{period}</p>
+      </div>
 
-      <div className="pl-5 sm:pl-0 sm:col-span-2 flex flex-col gap-2">
+      <div className="pl-6 sm:pl-0 sm:col-span-2 flex flex-col gap-2">
         <div className="flex flex-col gap-1">
           <p className="text-base md:text-lg font-semibold">{title}</p>
           <p className="text-sm md:text-base font-normal text-foreground/60 whitespace-pre-wrap">{sub_title}</p>
