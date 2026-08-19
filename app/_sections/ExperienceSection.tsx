bedffa10c62283caef4f7561f0f2c86089f58f71
박민규 <me@mingyu.dev>
2026-08-19 22:32:46 +0900

    Revert "fix: skip experience links that are missing a label or href"
    This reverts commit b0a48bed9bb8dc8d147407e967b31154e71c323c.

--- app/_sections/ExperienceSection.tsx
+++ app/_sections/ExperienceSection.tsx
@@ -24,9 +24,7 @@ async function getExperience(locale: string) {
   );
 
   return expWithSkills.map(({ links, ...res }) => ({
-    links: links
-      .map(link => parsePrismaJSON<Partial<{ href: string; label: string }>>(link))
-      .filter((link): link is { href: string; label: string } => Boolean(link?.href && link?.label)),
+    links: links.map(link => parsePrismaJSON<{ href: string; label: string }>(link)),
     ...res,
   }));
 }
